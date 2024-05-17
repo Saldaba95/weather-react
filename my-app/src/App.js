8a9ec952160212e7022920fb90c8961153d2c357
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-05-17 17:39:50 -0500

    add
    still messing with it to see how things work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello everyone </h1>
+        <Weather />
         <a
           className="App-link"
           href="https://reactjs.org"
